b308d945aaca5537fa56cb1fc2dacc079d61178c
Pauline Revilla <p_revilla@ymail.com>
2015-02-11 14:55:51 +0800

    Update customTag.js

--- scr/customTag.js
+++ scr/customTag.js
@@ -1,12 +1,15 @@
 varAppName
-
 .controller('TagController', [ '$scope', function($scope) {
 
   $scope.inputTags = [];
   $scope.tagText = '';
 
+  var self =this;
+
   this.init = function() {
-    $scope.$watch('tagText', function(val) {
+    initTags();
+
+    $scope.$watch('tags', function(val) {
       $scope.changeFn();
     });
   }
@@ -14,26 +17,37 @@ varAppName
   $scope.onKeyDown = function(event) {
     var keyCode = event.keyCode;
     if ($scope.tagText.length > 0 && keyCode === 9) {
+      //tab
       event.preventDefault();
       $scope.addTag();
     } else if ($scope.tagText.length === 0 && keyCode === 8) {
+      //backspace
       event.preventDefault();
       $scope.deleteTag();
+    } else if (keyCode === 32 || keyCode === 13 || keyCode === 188) {
+      //space, enter, comma
+      event.preventDefault();
+      if($scope.tagText.length > 0) {
+        $scope.addTag();
+      }
     }
   };
 
   $scope.onKeyUp = function(event) {
     var keyCode = event.keyCode;
-    if ($scope.tagText.length > 0) {
-      if (keyCode === 13 || keyCode === 32) {
-      // enter, space, comma
-        event.preventDefault();
-        $scope.addTag();
-      }
-      if (keyCode === 188) {
-        event.preventDefault();
-        $scope.addTag(true); 
-      }
+    if (keyCode === 8 || keyCode === 9 || keyCode === 13 || keyCode === 32 || keyCode === 188) {
+      event.preventDefault();
+    }
+  };
+
+  var initTags = function() {
+    if($scope.tags && $scope.tags.length > 0){
+      var newTags = $scope.tags.split(",");
+      _.each(newTags, function(tag){
+        $scope.inputTags = _.union($scope.inputTags, [tag.trim()]);
+      })
+      $scope.tagText = '';
+      updateTags();
     }
   };
 
@@ -46,15 +60,18 @@ varAppName
       $scope.tagText = $scope.tagText.slice(0, -1);
     }
 
-    $scope.inputTags.push({name: $scope.tagText});
+    $scope.inputTags = _.union($scope.inputTags, [$scope.tagText]);
     $scope.tagText = '';
 
     updateTags();
   };
 
-  $scope.deleteTag = function() {
-    $scope.inputTags.pop();
-
+  $scope.deleteTag = function(tag) {
+    if (tag) {
+      $scope.inputTags = _.without($scope.inputTags, tag);
+    } else {
+      $scope.inputTags.pop();
+    }
     updateTags();
   };
 
@@ -64,7 +81,7 @@ varAppName
       if (tags.length > 0) {
         tags += ',';
       }
-      tags += tag.name;
+      tags += tag;
     });
     $scope.tags = tags;
   };
@@ -73,12 +90,12 @@ varAppName
 
 }])
 
-.directive("customTag", function() {
+.directive('customTag', function() {
   return {
     restrict: 'AE',
-    template: '<div ng-repeat="(key, tag) in inputTags" class="input-tag">' +
-    '<span ng-bind="tag.name"></span>' + 
-    '<span class="fa fa-close" ng-click="deleteTag(key)"></span>' +
+    template: '<div ng-repeat="tag in inputTags" class="input-tag">' +
+    '<span ng-bind="tag"></span>' + 
+    '<span class="fa fa-close" ng-click="deleteTag(tag)"></span>' +
     '</div>' +
     '<input type="text" ng-model="tagText" ng-keyup="onKeyUp($event)" ng-keydown="onKeyDown($event)">',
     scope: {
